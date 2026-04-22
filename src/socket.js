2b9c27c79043bf0819a496cb0f0bdcde27f28b73
Andrea Michelle Olivas Rivera <andreaolivas@Andreas-MacBook-Pro-3.local>
2026-04-21 18:50:47 -0600

    socket

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,12 @@
 import { io } from 'socket.io-client';
 
-// La URL debe ser la de tu proyecto en Railway
-const URL = 'https://serverchatify-production-f701.up.railway.app'; 
+const URL = 'https://serverchatify-production-f701.up.railway.app';
 
 export const socket = io(URL, {
-  transports: ['websocket'] // <-- ESTA ES LA PIEZA QUE FALTA
+  // 1. Esto evita el error de "serverOffset"
+  auth: {
+    serverOffset: 0
+  },
+  // 2. Esto soluciona los problemas de conexión en Railway
+  transports: ['websocket']
 });
